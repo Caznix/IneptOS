a76f0da0887375c51b948e99445301a7ba082907
Colin Leftwich <Leftwichcolin@protonmail.com>
2024-09-29 10:13:51 -0500

    Make cloning limine run by default

--- kernel/build.rs
+++ kernel/build.rs
@@ -2,6 +2,12 @@ use std::env;
 use std::process::Command;
 
 fn main() {
+    let _ = Command::new("git")
+        .arg("submodule")
+        .arg("update")
+        .arg("--init")
+        .spawn();
+
     let arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
     println!("cargo:rustc-link-arg=-Tkernel/lds/{arch}-qemu.ld");
     println!("cargo:rerun-if-changed={arch}-qemu.ld");
