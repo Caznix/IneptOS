49550f5e4b24e3f6ed5cb968a1ef9094495011de
Colin Leftwich <LeftwichColin@protonmail.com>
2024-10-02 23:36:21 -0500

    Replace IneptOs kernel binary find with static path

--- xtask/src/kbuild.rs
+++ xtask/src/kbuild.rs
@@ -17,21 +17,7 @@ pub fn build_x86_64() {
         .arg("--target=x86_64-unknown-none")
         .output();
 
-    let target_dir = "target/x86_64-unknown-none/debug";
-
-    let find_output = Command::new("find")
-        .arg(&target_dir)
-        .arg("-maxdepth")
-        .arg("1")
-        .arg("-perm")
-        .arg("-111")
-        .arg("-type")
-        .arg("f")
-        .output();
-
-    let converted_stdout_result = String::from_utf8(find_output.unwrap().stdout);
-    let ineptos_binary_path = PathBuf::from(&converted_stdout_result.unwrap().trim());
-
+    let ineptos_binary_path = PathBuf::from("target/x86_64-unknown-none/debug/./ineptOS");
     fs::copy(&ineptos_binary_path, &PathBuf::from("kernel/kernel")).unwrap();
 }
 
