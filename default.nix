29109da0ed30b67e170a6612deba69bdf32dca63
Cazdotsys <Caznix01@gmail.com>
2024-10-02 23:55:31 -0400

    Add nixOS support

---
+++ default.nix
@@ -0,0 +1,30 @@
+/*
+based on
+https://discourse.nixos.org/t/how-can-i-set-up-my-rust-programming-environment/4501/9
+*/
+let
+  rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+  rustVersion = "latest";
+  #rustVersion = "1.83.0";
+  rust = pkgs.rust-bin.nightly.${rustVersion}.default.override {
+    extensions = [
+      "rust-src" # for rust-analyzer
+      "rust-analyzer"
+    ];
+  };
+in
+pkgs.mkShell {
+  buildInputs = [
+    rust
+  ] ++ (with pkgs; [
+    pkg-config
+    libisoburn
+    mold
+    # other dependencies
+    #gtk3
+    #wrapGAppsHook
+
+  ]);
+  RUST_BACKTRACE = 1;
+}
